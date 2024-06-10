56241fc05cdcdbd069c9b0c4d2bdc87b7c36820c
Artem Novok <aan200545@gmail.com>
2024-06-10 12:54:08 -0700

    token creation

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
-func main() {
-	//TODO get token
+import (
+	"flag"
+	"log"
+)
 
+func main() {
+	token := mustToken()
+	_ = token
 	//TODO init client
 
 	//TODO init fetcher
@@ -10,4 +15,14 @@ func main() {
 	//TODO init processor
 
 	//TODO init consumer and start consumer
+}
+
+func mustToken() string {
+	var token string
+	flag.StringVar(&token, "token", "", "token for telegram client")
+	flag.Parse()
+	if token == "" {
+		log.Fatal("empty token string")
+	}
+	return token
 }
